ff7ef808f7a007efc84deef556b12e1701e5d575
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-02-05 12:35:30 +0100

    fixed krupp'2 gcc compiler version

--- krupp2/ctestscript-gcc-debug.cmake
+++ krupp2/ctestscript-gcc-debug.cmake
@@ -7,7 +7,7 @@ set(CTEST_BUILD_OPTIONS "")
 SET(ENV{CC} /usr/bin/gcc)
 SET(ENV{CXX} /usr/bin/g++)
 set(COMPILER_NAME "GCC")
-set(COMPILER_VERSION "4.8.1")
+set(COMPILER_VERSION "4.8.2")
 set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
 set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
 set(WITH_MEMCHECK FALSE)
