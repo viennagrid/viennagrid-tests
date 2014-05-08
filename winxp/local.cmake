23f09da4af09c5d0294068984d1a1794624b0eb0
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-05-08 15:00:15 +0200

    removed unsupported paramters for Windows

--- winxp/local.cmake
+++ winxp/local.cmake
@@ -2,7 +2,3 @@ set(ROOT_DIRECTORY "D:/git/ViennaGrid")
 set(CTEST_SOURCE_DIRECTORY $ENV{VIENNAGRIDPATH})
 set(USED_PROJECTS "VIENNAGRID")
 set(CTEST_BUILD_OPTIONS "")
-
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
-set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
